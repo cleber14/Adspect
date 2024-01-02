e12fb40961a72ce0ae35480c03413c5e52dea799
Cleber Souza <102312146+cleber14@users.noreply.github.com>
2024-01-01 23:39:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-let currentSlide = 1
+window.onload = function() {
+    let currentSlide = 1;
+    let lateralButton = document.querySelector(".lateral-menu");
+    let lateralBar = document.getElementById("container-lateral-menu");
 
-function clique() {
-    alert("working")
-}
+    lateralButton.addEventListener("click", clique);
 
-function showSlide(n) {
-    let slides = document.getElementsByClassName("images")
-
-    if (n > slides.length) {
-        currentSlide = 1;
-    } else if (n < 1) {
-        currentSlide = slides.length;
+    function clique() {
+        lateralBar.style.display = 'block';
+        
     }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+
+    function showSlide(n) {
+        let slides = document.getElementsByClassName("images");
+
+        if (n > slides.length) {
+            currentSlide = 1;
+        } else if (n < 1) {
+            currentSlide = slides.length;
+        }
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[currentSlide - 1].style.display = "block";
     }
-    slides[currentSlide - 1].style.display = "block";
-}
 
-function changeSlide(n) {
-    showSlide(currentSlide += n);
-}
+    function changeSlide(n) {
+        showSlide(currentSlide += n);
+    }
 
-// Inicializa o slide
-showSlide(currentSlide);
+    // Inicializa o slide
+    showSlide(currentSlide);
+};
